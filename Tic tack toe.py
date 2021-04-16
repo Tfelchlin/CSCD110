b0720be65971f900ea717e22a98df0a0afddd67f
Tfelchlin <81547488+Tfelchlin@users.noreply.github.com>
2021-04-15 20:43:48 -0700

    Add files via upload

---
+++ Tic tack toe.py
@@ -0,0 +1,9 @@
+Theboard = {'1': ' ' , '2': ' ' , '3': ' ' ,
+             '4': ' ' , '5': ' ' , '6': ' ' ,
+             '7': ' ' , '8': ' ' , '9': ' ' ,}
+def printboard(board):
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
