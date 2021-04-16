d89c3d9d9bc4e9ca2e9d64fb764e49adc6ae57fd
Tfelchlin <81547488+Tfelchlin@users.noreply.github.com>
2021-04-15 20:43:04 -0700

    Add files via upload

---
+++ Anaconda.py
@@ -0,0 +1,11 @@
+# Get two towns fronm user, Montana
+town1 = input('enter a town in montana: ')
+town2 = input('enter another town in montana: ')
+# Sort Alphabetical order
+if town1 < town2:
+    print(town1)
+    print(town2)
+else:
+    print(town2)
+    print(town1)
+print('Montana is the last best place')
---
+++ lab 1.py
@@ -0,0 +1,3 @@
+age= int(input("Enter your age:"))
+Hour = age * 365 * 24
+print("You have lived", Hour,"Hours")
